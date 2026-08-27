5ffd76810f8820f53377c3087ba9da02c9d227b6
Developer <developer@example.com>
2026-08-28 02:58:59 +0500

    Fade out photo before WHAT I DO section to prevent overlapping text

--- src/components/ProfileImage.tsx
+++ src/components/ProfileImage.tsx
@@ -42,6 +42,7 @@ const ProfileImage = () => {
       tl2
         .to(".about-section", { y: "30%", duration: 6 }, 0)
         .to(".about-section", { opacity: 0, delay: 3, duration: 2 }, 0)
+        .to(".profile-model", { opacity: 0, delay: 4, duration: 2 }, 0)
         .fromTo(
           ".profile-model",
           { pointerEvents: "inherit" },
@@ -75,7 +76,7 @@ const ProfileImage = () => {
         .fromTo(
           ".profile-model",
           { y: "0%" },
-          { y: "-150vh", duration: 4, ease: "none", delay: 1 },
+          { y: "-50vh", duration: 2, ease: "power2.out", delay: 0 },
           0
         )
         .fromTo(".whatIDO", { y: 0 }, { y: "15%", duration: 2 }, 0);
